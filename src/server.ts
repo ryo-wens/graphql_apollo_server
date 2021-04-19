fcff8a0b569d1d6b63ea6ee5dd7fd5c2dde8b8da
github user ryo-wens <ryousuke.11460@gmail.com>
2021-04-19 22:01:40 +0900

    Add an id to User to extend Users

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,39 @@
 import express from 'express';
 import cors from 'cors';
-import {ApolloServer, gql} from 'apollo-server-express';
+import {ApolloServer, gql, IResolvers} from 'apollo-server-express';
+import {Users} from './types'
 
 const app = express();
 
 app.use(cors());
 
+const users : Users = {
+    '1': {id:1, userName: 'Holly Q'},
+    '2': {id:2, userName: 'IO'}
+}
+
+const me = users[1]
+
 const schema = gql`
     type User {
+        id: ID!
         userName: String!
+        firstName: String!
+        lastName: String!
     }
     
     type Query {
         me: User
+        users: [User!]
+        user(id: ID!): User
     }
 `;
 
-const resolvers =  {
+const resolvers: IResolvers =  {
     Query: {
-        me: () => ({userName: 'Holly Q'})
+        me: () => me,
+        users: () => Object.values(users),
+        user: (parent, { id }) => users[id] || null,
     }
 };
 
